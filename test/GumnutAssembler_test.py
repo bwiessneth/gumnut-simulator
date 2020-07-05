6219ca586f947b2d7c29a160997ca387849c3715
Benjamin Wiessneth <b.wiessneth@googlemail.com>
2020-07-05 12:11:07 +0200

    Restructured tests

--- test/GumnutAssembler_test.py
+++ test/GumnutAssembler_test.py
@@ -3,7 +3,6 @@ __author__ = "BW"
 import os
 import sys
 import hashlib
-import unittest
 import pytest
 
 sys.path.insert(0, os.getcwd())  # Add current directory to PYTHONPATH
@@ -69,18 +68,7 @@ def test_check_number_floats(gass):
     assert gass._check_number("-0,123") == -1
 
 
-def test_extract_identifier_from_line(gass):
-    # ..
-    assert gass._extract_identifier_from_line("; Comment org 0x1") == GasmLine(None, None, None, None, None)
-
-    # ..
-    assert gass._extract_identifier_from_line("org 0x1") == GasmLine(None, "org", 1, None, None)
-    assert gass._extract_identifier_from_line("bigdec1024: byte 1024") == GasmLine(
-        "bigdec1024", "byte", 1024, None, None
-    )
-    assert gass._extract_identifier_from_line("null0: byte 0") == GasmLine("null0", "byte", 0, None, None)
-    assert gass._extract_identifier_from_line("neg_1: byte -1") == GasmLine("neg_1", "byte", 0xFF, None, None)
-
+def test_extract_identifier_from_line_arithmetic_instructions(gass):
     # Arithemtic and logical instructions
     # Register access
     assert gass._extract_identifier_from_line("add r0, r0, r0") == GasmLine(None, "add", "r0", "r0", "r0")
@@ -105,12 +93,16 @@ def test_extract_identifier_from_line(gass):
     assert gass._extract_identifier_from_line("xor r0, r1, 0x12") == GasmLine(None, "xor", "r0", "r1", 18)
     assert gass._extract_identifier_from_line("mask r0, r1, 0x12") == GasmLine(None, "mask", "r0", "r1", 18)
 
+
+def test_extract_identifier_from_line_shift_instructions(gass):
     # Shift instructions
     assert gass._extract_identifier_from_line("shl r1, r2, 0x2") == GasmLine(None, "shl", "r1", "r2", 2)
     assert gass._extract_identifier_from_line("shr r1, r2, 0x2") == GasmLine(None, "shr", "r1", "r2", 2)
     assert gass._extract_identifier_from_line("rol r1, r2, 0x2") == GasmLine(None, "rol", "r1", "r2", 2)
     assert gass._extract_identifier_from_line("ror r1, r2, 0x2") == GasmLine(None, "ror", "r1", "r2", 2)
 
+
+def test_extract_identifier_from_line_memory_io_instructions(gass):
     # Memory and I/O instructions
     # Direct access
     assert gass._extract_identifier_from_line("ldm r1, 0x12") == GasmLine(None, "ldm", "r1", 18, None)
@@ -132,35 +124,48 @@ def test_extract_identifier_from_line(gass):
         None, "ldm", "r1", "start_val", 0xEE
     )
 
+
+def test_extract_identifier_from_line_branch_instructions(gass):
     # Branch instructions
     assert gass._extract_identifier_from_line("bz 12") == GasmLine(None, "bz", 12, None, None)
-    # self.assertEqual(parser._extract_identifier_from_line("bz -12"),parser.ASMLINE(None,"bz","-12",None,None))
+    assert gass._extract_identifier_from_line("bz -12") == GasmLine(None, "bz", 244, None, None)
     assert gass._extract_identifier_from_line("bnz 12") == GasmLine(None, "bnz", 12, None, None)
-    # self.assertEqual(parser._extract_identifier_from_line("bnz -12"),parser.ASMLINE(None,"bnz","-12",None,None))
+    assert gass._extract_identifier_from_line("bnz -12") == GasmLine(None, "bnz", 244, None, None)
     assert gass._extract_identifier_from_line("bc 12") == GasmLine(None, "bc", 12, None, None)
-    # self.assertEqual(parser._extract_identifier_from_line("bc -12"),parser.ASMLINE(None,"bc","-12",None,None))
+    assert gass._extract_identifier_from_line("bc -12") == GasmLine(None, "bc", 244, None, None)
     assert gass._extract_identifier_from_line("bnc 12") == GasmLine(None, "bnc", 12, None, None)
-    # self.assertEqual(parser._extract_identifier_from_line("bnc -12"),parser.ASMLINE(None,"bnc","-12",None,None))
+    assert gass._extract_identifier_from_line("bnc -12") == GasmLine(None, "bnc", 244, None, None)
 
+
+def test_extract_identifier_from_line_jump_instructions(gass):
     # Jump instructions
     assert gass._extract_identifier_from_line("jmp 0xFC12") == GasmLine(None, "jmp", 64530, None, None)
     assert gass._extract_identifier_from_line("jsb 0xFC12") == GasmLine(None, "jsb", 64530, None, None)
 
+
+def test_extract_identifier_from_line_misc_instructions(gass):
     # Empty lines, comments and labels
     assert gass._extract_identifier_from_line("") == GasmLine(None, None, None, None, None)
     assert gass._extract_identifier_from_line(";Comment") == GasmLine(None, None, None, None, None)
+    assert gass._extract_identifier_from_line(" ; Comment") == GasmLine(None, None, None, None, None)
+    assert gass._extract_identifier_from_line("; Comment org 0x1") == GasmLine(None, None, None, None, None)
+    assert gass._extract_identifier_from_line(" ; Comment org 0x1") == GasmLine(None, None, None, None, None)
     assert gass._extract_identifier_from_line("label:") == GasmLine("label", None, None, None, None)
     assert gass._extract_identifier_from_line("label: add r0, r1, r2") == GasmLine("label", "add", "r0", "r1", "r2")
     assert gass._extract_identifier_from_line("label: bz 0x12") == GasmLine("label", "bz", 18, None, None)
     assert gass._extract_identifier_from_line("label: ldm r1, 0x12") == GasmLine("label", "ldm", "r1", 18, None)
     assert gass._extract_identifier_from_line("label: add r0, r1, r2") == GasmLine("label", "add", "r0", "r1", "r2")
     assert gass._extract_identifier_from_line("label: out r1, (r2) + 0x12") == GasmLine("label", "out", "r1", "r2", 18)
-    
-    # gass._extract_identifier_from_line("char_a: ascii 'a'")
+
+    assert gass._extract_identifier_from_line("org 0x1") == GasmLine(None, "org", 1, None, None)
+    assert gass._extract_identifier_from_line("bigdec1024: byte 1024") == GasmLine(
+        "bigdec1024", "byte", 1024, None, None
+    )
+    assert gass._extract_identifier_from_line("null0: byte 0") == GasmLine("null0", "byte", 0, None, None)
+    assert gass._extract_identifier_from_line("neg_1: byte -1") == GasmLine("neg_1", "byte", 0xFF, None, None)
 
     with pytest.raises(GumnutExceptions.InvalidInstruction):
         gass._extract_identifier_from_line("zyxq")
-
 
 
 def test_objectcode_comparison_static(gass):
@@ -268,7 +273,7 @@ def test_get_reference(gass):
     # ..
 
 
-def test_assemble_source_line(gass):
+def test_assemble_source_line_arithmetic_instructions(gass):
     # Arithemtic and logical instructions
     # Register access
     assert gass._assemble_source_line(GasmLine(None, "add", "r1", "r2", "r3")) == 0x38A60
@@ -291,12 +296,16 @@ def test_assemble_source_line(gass):
     assert gass._assemble_source_line(GasmLine(None, "xor", "r1", "r2", 0x12)) == 0x18A12
     assert gass._assemble_source_line(GasmLine(None, "mask", "r1", "r2", 0x12)) == 0x1CA12
 
+
+def test_assemble_source_line_shift_instructions(gass):
     # Shift instructions
     assert gass._assemble_source_line(GasmLine(None, "shl", "r1", "r2", 0x12)) == 0x30A40
     assert gass._assemble_source_line(GasmLine(None, "shr", "r1", "r2", 0x12)) == 0x30A41
     assert gass._assemble_source_line(GasmLine(None, "rol", "r1", "r2", 0x12)) == 0x30A42
     assert gass._assemble_source_line(GasmLine(None, "ror", "r1", "r2", 0x12)) == 0x30A43
 
+
+def test_assemble_source_line_memory_io_instructions(gass):
     # Memory and I/O instructions
     # Direct offset
     assert gass._assemble_source_line(GasmLine(None, "ldm", "r1", 0x12, None)) == 0x20812
@@ -309,6 +318,8 @@ def test_assemble_source_line(gass):
     assert gass._assemble_source_line(GasmLine(None, "inp", "r1", "r2", 0x12)) == 0x28A12
     assert gass._assemble_source_line(GasmLine(None, "out", "r1", "r2", 0x12)) == 0x2CA12
 
+
+def test_assemble_source_line_branch_instructions(gass):
     # Branch instructions
     assert gass._assemble_source_line(GasmLine(None, "bz", 0x00, None, None)) == 0x3E0FF
     assert gass._assemble_source_line(GasmLine(None, "bz", 0x01, None, None)) == 0x3E000
@@ -342,6 +353,8 @@ def test_assemble_source_line(gass):
     assert gass._assemble_source_line(GasmLine(None, "bnc", 0x01, None, None)) == 0x3ECF0
     assert gass._assemble_source_line(GasmLine(None, "bnc", 0x80, None, None)) == 0x3EC6F
 
+
+def test_assemble_source_line_jump_instructions(gass):
     # Jump instructions
     assert gass._assemble_source_line(GasmLine(None, "jmp", 0x00, None, None)) == 0x3C000
     assert gass._assemble_source_line(GasmLine(None, "jmp", 0xFFF, None, None)) == 0x3CFFF
@@ -350,6 +363,8 @@ def test_assemble_source_line(gass):
     assert gass._assemble_source_line(GasmLine(None, "jsb", 0xFFF, None, None)) == 0x3DFFF
     assert gass._assemble_source_line(GasmLine(None, "jsb", 0xFFFF, None, None)) == 0x3DFFF
 
+
+def test_assemble_source_line_misc_instructions(gass):
     # Misc instructions
     assert gass._assemble_source_line(GasmLine(None, "ret", None, None, None)) == 0x3F000
     assert gass._assemble_source_line(GasmLine(None, "reti", None, None, None)) == 0x3F100
@@ -357,7 +372,3 @@ def test_assemble_source_line(gass):
     assert gass._assemble_source_line(GasmLine(None, "disi", None, None, None)) == 0x3F300
     assert gass._assemble_source_line(GasmLine(None, "wait", None, None, None)) == 0x3F400
     assert gass._assemble_source_line(GasmLine(None, "stby", None, None, None)) == 0x3F500
-
-
-if __name__ == "__main__":
-    unittest.main()  # run all tests
