cc3bb73ab4dc41b9277af0c28c149e526817a5d4
Benjamin Wiessneth <b.wiessneth@googlemail.com>
2020-07-06 19:42:15 +0200

    Linting.. -.-

--- test/GumnutAssemblerCLI_test.py
+++ test/GumnutAssemblerCLI_test.py
@@ -1,17 +1,25 @@
 import os
 
+
 def test_help():
-	exit_status = os.system(r'python GumnutSimulator\GumnutAssembler.py --help')
-	assert exit_status == 0
+    exit_status = os.system(r"python GumnutSimulator\GumnutAssembler.py --help")
+    assert exit_status == 0
+
 
 def test_cli():
-	exit_status = os.system(r'python GumnutSimulator\GumnutAssembler.py test\asm_source\sample.gsm')
-	assert exit_status == 0
+    exit_status = os.system(r"python GumnutSimulator\GumnutAssembler.py test\asm_source\sample.gsm")
+    assert exit_status == 0
+
 
 def test_cli_multiple():
-	exit_status = os.system(r'python GumnutSimulator\GumnutAssembler.py test\asm_source\sample.gsm test\asm_source\jmp.gsm')
-	assert exit_status == 0	
+    exit_status = os.system(
+        r"python GumnutSimulator\GumnutAssembler.py test\asm_source\sample.gsm test\asm_source\jmp.gsm"
+    )
+    assert exit_status == 0
+
 
 def test_cli_out_dir():
-	exit_status = os.system(r'python GumnutSimulator\GumnutAssembler.py test\asm_source\sample.gsm -o test\asm_output_cli')
-	assert exit_status == 0
+    exit_status = os.system(
+        r"python GumnutSimulator\GumnutAssembler.py test\asm_source\sample.gsm -o test\asm_output_cli"
+    )
+    assert exit_status == 0
