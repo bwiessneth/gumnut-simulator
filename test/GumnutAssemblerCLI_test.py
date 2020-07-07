c75935e31e169fab984c7be34b34933e8f085677
Benjamin Wiessneth <b.wiessneth@googlemail.com>
2020-07-07 06:45:32 +0200

    Removed multiple sources test

--- test/GumnutAssemblerCLI_test.py
+++ test/GumnutAssemblerCLI_test.py
@@ -11,13 +11,6 @@ def test_cli():
     assert exit_status == 0
 
 
-def test_cli_multiple():
-    exit_status = os.system(
-        r"python GumnutSimulator\GumnutAssembler.py test\asm_source\sample.gsm test\asm_source\jmp.gsm"
-    )
-    assert exit_status == 0
-
-
 def test_cli_out_dir():
     exit_status = os.system(
         r"python GumnutSimulator\GumnutAssembler.py test\asm_source\sample.gsm -o test\asm_output_cli"
